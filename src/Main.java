720fedd3e836ecdf483c46a44d0113cd36783878
alexander <petrovodessa1936@gmail.com>
2022-07-08 03:23:20 +0300

    comments and tests for consistency

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,39 @@ public class Main {
         Dog dog3 = new Dog("Sharik",5,"Black");
         Dog dog4 = new Dog("Sharik",5,"Black");
 
-
+        //test for diff objects
         System.out.println(cat1.equals(cat2)+"  "+cat1.hashCode()+"  "+cat2.hashCode());
+        //test for same objects
         System.out.println(cat1.equals(cat3)+"  "+cat1.hashCode()+"  "+cat3.hashCode());
+        //test for symmetry
         System.out.println(cat3.equals(cat1)+"  "+cat3.hashCode()+"  "+cat1.hashCode());
+        //test for transitivity
         System.out.println(cat3.equals(cat4)+"  "+cat3.hashCode()+"  "+cat4.hashCode());
+        //test for revlexity
         System.out.println(cat1.equals(cat1));
+        //test for consistency
+        System.out.println(cat1.equals(cat3)+"  "+cat1.hashCode()+"  "+cat3.hashCode());
         System.out.println();
 
+        //test for diff objects
         System.out.println(dog1.equals(dog2)+"  "+dog1.hashCode()+"  "+dog2.hashCode());
+        //test for same objects
         System.out.println(dog1.equals(dog3)+"  "+dog1.hashCode()+"  "+dog3.hashCode());
+        //test for symmetry
         System.out.println(dog3.equals(dog1)+"  "+dog3.hashCode()+"  "+dog1.hashCode());
+        //test for transitivity
         System.out.println(dog3.equals(dog4)+"  "+dog3.hashCode()+"  "+dog4.hashCode());
+        //test for revlexity
         System.out.println(dog1.equals(dog1));
+        //test for consistency
+        System.out.println(dog1.equals(dog3)+"  "+dog1.hashCode()+"  "+dog3.hashCode());
         System.out.println();
 
+        //test for diff classes
         System.out.println(cat1.equals(dog1));
+        //tests for null
+        System.out.println(cat1.equals(null));
+        System.out.println(dog1.equals(null));
 
 
     }
